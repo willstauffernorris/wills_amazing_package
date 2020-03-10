34ac69f2b113a71df1f2bc78da2a49ca38bd47f7
willsn <willsn@gmail.com>
2020-03-10 15:33:52 -0700

    trying build again

--- package/my_mod.py
+++ package/my_mod.py
@@ -1,25 +1,19 @@
+
+import pandas as import pd
 '''
 test function
 '''
 
-
 def mega(x):
     return x *100
 
-
-
-##------------------
-###Make a "Date" splitter
-
-### x must be a 'date' column in a dataframe
-
 '''
-def date_splitter(x):
-    df[x] = pd.to_datetime(df['Date'], format='%Y%m%d')
-    df['Year'] = df['Date'].dt.year
-    df['Month']= df['Date'].dt.month
-    df['Day']= df['Date'].dt.day
-    df['Day_of_year'] = ((df['Month']*30.333-30).round(0) + df['Day'])
-
-x = input()
+Single function to take a list, turn it into a series and add it to a dataframe as a new column
 '''
+def list_to_column(x):
+    '''
+    x must be a list in this format: ['item a', 'item b', 'item c']
+    '''
+    series = pd.Series(x)
+    frame = series.to_frame()
+    return frame
--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import find_packages, setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
-    name="s2t2-lambdata-12",
+    name="wills_amazing_package",
     version="1.0",
-    author="MJ Rossetti",
-    author_email="datacreativellc@gmail.com",
-    description="For example purposes",
+    author="Will SN",
+    author_email="willsn@gmail.com",
+    description="Testing packages",
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
     #license="MIT",
-    url="https://github.com/s2t2/lambdata-12y",
+    url="https://github.com/willstauffernorris/wills_amazing_package",
     #keywords="",
     packages=find_packages() # ["my_lambdata"]
 )
