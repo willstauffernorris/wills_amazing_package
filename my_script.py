fc43b36d92591412941dade57657ed772b38427b
willsn <willsn@gmail.com>
2020-03-09 21:39:36 -0700

    first commit

---
+++ my_script.py
@@ -0,0 +1,16 @@
+###Make a "Date" splitter
+import pandas as pd
+### x must be a 'date' column in a dataframe
+
+'''
+def date_splitter(x):
+    df[x] = pd.to_datetime(df['Date'], format='%Y%m%d')
+    df['Year'] = df['Date'].dt.year
+    df['Month']= df['Date'].dt.month
+    df['Day']= df['Date'].dt.day
+    df['Day_of_year'] = ((df['Month']*30.333-30).round(0) + df['Day'])
+
+x = input()
+'''
+
+print('fuck ya')
